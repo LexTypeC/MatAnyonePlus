4faf9e5fd9035a9bf21538eef4767c16bcaf99ff
ai_anchorite <ai_anchorite@proton.me>
2025-02-21 19:25:05 +1300

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "3.2",
   title: "MatAnyone",
-  description: "",
+  description: "https://github.com/pq-yang/MatAnyone.git",
   icon: "icon.png",
   menu: async (kernel, info) => {
     let installed = info.exists("app/env")
