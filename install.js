9a4ca8df65ceaca43bfa4de5d23a0b7b181cad53
ai_anchorite <ai_anchorite@proton.me>
2025-02-21 17:27:38 +1300

    Add files via upload

---
+++ install.js
@@ -0,0 +1,50 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/pq-yang/MatAnyone.git app",
+        ]
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "app.py",
+        dest: "app/hugging_face/app.py"
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "requirements.txt",
+        dest: "app/requirements.txt"
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install -r requirements.txt"
+        ]
+      }
+    }
+  ]
+}
